b31b3dd37045a5048bdbc15584f915c96ac61b30
miio <info@miio.info>
2015-02-17 13:42:57 +0900

    get value pattern accept ","

--- lib/apple_manifest_rails/enroll/response_parser.rb
+++ lib/apple_manifest_rails/enroll/response_parser.rb
@@ -8,12 +8,12 @@ module AppleManifestRails
       end
 
       def get key
-        self.body.match(/<key>#{key}<key><string>([a-zA-Z0-9]+)<string>/)[1]
+        self.body.match(/<key>#{key}<key><string>([a-zA-Z0-9,]+)<string>/)[1]
       end
 
       private
       def deobfuscate(input)
-        regex = /[A-Za-z0-9]|\>|\<|\?|\!|\"/
+        regex = /[A-Za-z0-9,]|\>|\<|\?|\!|\"/
         input.chars.select{|i| i.match(regex) }.join
       end
     end
